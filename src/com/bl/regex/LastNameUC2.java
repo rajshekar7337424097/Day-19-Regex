767675cdc4c2c06e5a85b5c48c0f32e7742765c0
rajashekar reddy <chrajashekarreddy777@gmail.com>
2021-12-30 17:39:02 +0530

    creating last name using regex

--- src/com/bl/regex/LastNameUC2.java
+++ src/com/bl/regex/LastNameUC2.java
@@ -4,21 +4,20 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class FirstNameUC1 {
+public class LastNameUC2 {
 	public static void main(String[] args) {
 		String namePattern = "^[A-Z]{1}[a-z]{3,9}$";
 		Pattern regex = Pattern.compile(namePattern);
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the First Name :");
+		System.out.println("Enter the Last Name :");
 		String inputStr = sc.nextLine();
-		Matcher firstNameMatcher = regex.matcher(inputStr);
+		Matcher lastNameMatcher = regex.matcher(inputStr);
 		sc.close();
-		if(firstNameMatcher.matches()) {
+		if(lastNameMatcher.matches()) {
 			System.out.println(inputStr+" is valid Name");
 		}else {
 			System.out.println(inputStr+ " is invalid Name");
 		}
 	}
-
 }
