d5cf10d9fecd3caac171c7b4591fd79442820c19
rajashekar reddy <chrajashekarreddy777@gmail.com>
2021-12-31 10:32:40 +0530

    creating password according to rule 2

--- PasswordUC6.java
+++ PasswordUC6.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PasswordUC5 {
+public class PasswordUC6 {
 	public boolean passwordRule1(String inputStr) {
-		String passwordPattern = "^[A-Za-z]{8}$";
+		String passwordPattern = "^(?=.*[A-Z])[A-Za-z]{8}$";
 		Pattern regex = Pattern.compile(passwordPattern);
 		Matcher passwordMatcher = regex.matcher(inputStr);
 		if(passwordMatcher.matches()) {
-			System.out.println(inputStr+"Enter password is valid ");
+			System.out.println(inputStr+"Enter password as per rule 2 ");
 			
 		}else {
 			System.out.println(inputStr+"Invalid Password");
@@ -21,7 +21,7 @@ public class PasswordUC5 {
 	public static void main(String[] args) {
 		PasswordUC5 password = new PasswordUC5();
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter 8 characher password for Login");
+		System.out.println("Enter 8 characher  with one Upper case password for Login");
 		String inputStr= sc.nextLine();
 		sc.close();
 		password.passwordRule1(inputStr);
--- PhoneNumber.java
+++ PhoneNumber.java
@@ -1,5 +1,25 @@
 package com.bl.regex;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PhoneNumber {
+	public static void main(String[] args) {
+		 
+		String phoneNumber = "^[0-9]{2}\\s{1}[0-9]{10}$";
+		Pattern regex = Pattern.compile(phoneNumber);
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the PhoneNumber :");
+		String inputStr = sc.nextLine();
+		Matcher phoneNumMatcher = regex.matcher(inputStr);
+		sc.close();
+		if(phoneNumMatcher.matches()) {
+			System.out.println(inputStr+"valid Phone Number");
+		}else {
+			System.out.println(inputStr+"Invalid Phone Number");
+		}
+	}
 
 }
